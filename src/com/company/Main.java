fae28cccf490337562547cb887a370f1059a8ac5
Elmirbek2005 <elmirbek2005.kg@gmail.com>
2022-02-11 15:30:18 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String nodb = "dsfsdfdsf";
+        int NUM = 0;
+        String word = "lol";
+        NUM += 8;
+        if (NUM < 0) {
+            System.out.println("Вы сохранили отрицательное число");
+        }else if (NUM > 0) {
+            System.out.println("Вы сохранили положительное число");
+        }else {
+            System.out.println("Вы сохранили нуль");
+        }
+    }
+}
